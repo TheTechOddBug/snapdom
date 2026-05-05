b222b1b3014baad09542097da0f09b8016bd1979
Juan Martin <martinmuda@gmail.com>
2026-05-05 14:50:17 -0400

    chore(eslint): ignore dist/, node_modules/, packages/**/dist/

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -2,6 +2,9 @@ const js = require("@eslint/js")
 const globals = require("globals")
 
 module.exports = [
+  {
+    ignores: ["dist/**", "node_modules/**", "packages/**/dist/**"]
+  },
   js.configs.recommended,
   {
     files: ["__tests__/**/*.js","src/**/*.js"],
